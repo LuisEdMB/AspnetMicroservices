71e828cf149445ad508f565578dd59eb67aff526
LuisEdMB <luis.mamanibedregal@externos-pe.cencosud.com>
2021-05-20 20:19:17 -0500

    First commit Basket API.

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string  UserName { get; set; }
+
+        public List<ShoppingCartItem> Items { get; set; }
+
+        public ShoppingCart()
+        {
+            Items = new List<ShoppingCartItem>();
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                var totalPrice = 0m;
+                Items?.ForEach(g => {
+                    totalPrice += g.Price * g.Quantity;
+                });
+                return totalPrice;
+            }
+        }
+    }
+}
