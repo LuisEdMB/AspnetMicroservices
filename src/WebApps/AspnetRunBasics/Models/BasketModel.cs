2768b42fd173aa8b3037894f175fe315fdcce7c3
LuisEdMB <luis.mamanibedregal@externos-pe.cencosud.com>
2021-07-01 12:15:10 -0500

    Finish code Web app.

---
+++ src/WebApps/AspnetRunBasics/Models/BasketModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace AspnetRunBasics.Models
+{
+    public class BasketModel
+    {
+        public string UserName { get; set; }
+        public List<BasketItemModel> Items { get; set; }
+        public decimal TotalPrice { get; set; }
+        public BasketModel()
+        {
+            Items = new List<BasketItemModel>();
+        }
+    }
+}
